fb2eefb82bb05dabd1dd951cddf223aa054fbe72
Denis <ivdenvas@gmail.com>
2022-11-24 16:00:10 +0300

    finish task 29

---
+++ Task 29/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("Введите элементы массива: ");
+
+int[] array1 = new int[8];
+
+FillArray(array1);
+PrintArray(array1);
+Console.Write($" -> ");
+PrintArray2(array1);
+
+void FillArray(int[] array)
+{
+    for (int i=0; i < array.Length; i++)
+        array[i] = int.Parse(Console.ReadLine() ?? "");
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i=0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+}
+
+
+void PrintArray2(int[] array)
+{
+    Console.Write($"[");
+    for (int i=0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+        
+    Console.Write($"]");
+}
